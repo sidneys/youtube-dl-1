b602d78fb042c728d10a1f36c63ac33b965eda3c
sidneys <sidneys.github.io@outlook.com>
2021-05-17 18:18:37 +0200

    [massengeschmack.tv] adds http download formats, api metadata querying

--- youtube_dl/extractor/massengeschmacktv.py
+++ youtube_dl/extractor/massengeschmacktv.py
@@ -8,14 +8,18 @@ from .common import InfoExtractor
 from ..utils import (
     clean_html,
     determine_ext,
+    ExtractorError,
     int_or_none,
     js_to_json,
     mimetype2ext,
-    parse_filesize,
+    try_get,
     unified_strdate,
-    ExtractorError,
 )
 
+from youtube_dl.utils import (
+    parse_resolution,
+    write_string,
+)
 
 class MassengeschmackTVIE(InfoExtractor):
     IE_NAME = 'massengeschmack.tv'
@@ -37,12 +41,10 @@ class MassengeschmackTVIE(InfoExtractor):
     }
 
     def _real_extract(self, url):
-        # Metadata
-        episode = self._match_id(url)
+        video_id = self._match_id(url)
+        webpage = self._download_webpage(url, video_id)
 
-        webpage = self._download_webpage(url, episode)
-
-        # Errorhandling
+        # errorhandling
         ERRORS = {
             r'<form id="register-form" action="/register':
             'Login required to access %s.',
@@ -50,8 +52,9 @@ class MassengeschmackTVIE(InfoExtractor):
 
         for error_re, error_msg in ERRORS.items():
             if re.search(error_re, webpage):
-                raise ExtractorError(error_msg % episode, expected=True)
+                raise ExtractorError(error_msg % video_id, expected=True)
 
+        # metadata lookup
         thumbnail = self._search_regex(r'POSTER\s*=\s*"([^"]+)', webpage, 'thumbnail', fatal=False)
 
         title = self._html_search_regex(
@@ -72,14 +75,14 @@ class MassengeschmackTVIE(InfoExtractor):
             r'(?s)<h6\b[^>]+\bid=["\']clip-releasetime[^>]+>(.+?)</span>',
             webpage, 'releasetime', fatal=False)
 
-        # set correct locale for date parsing
+        # provide de_DE locale for parsing of timestamp
         locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
 
         upload_date = unified_strdate(datetime.datetime.strptime(
             releasetime, '%d. %B %Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S'))
 
-        # Formats
-        sources = self._parse_json(self._search_regex(r'(?s)MEDIA\s*=\s*(\[.+?\]);', webpage, 'media'), episode, js_to_json)
+        # format (for streaming) lookup: webpage 
+        sources = self._parse_json(self._search_regex(r'(?s)MEDIA\s*=\s*(\[.+?\]);', webpage, 'media'), video_id, js_to_json)
 
         formats = []
         for source in sources:
@@ -90,7 +93,7 @@ class MassengeschmackTVIE(InfoExtractor):
             ext = determine_ext(furl) or mimetype2ext(source.get('type'))
             if ext == 'm3u8':
                 formats.extend(self._extract_m3u8_formats(
-                    furl, episode, 'mp4', 'm3u8_native',
+                    furl, video_id, 'mp4', 'm3u8_native',
                     m3u8_id='hls', fatal=False))
             else:
                 formats.append({
@@ -98,24 +101,55 @@ class MassengeschmackTVIE(InfoExtractor):
                     'format_id': determine_ext(furl),
                 })
 
-        for (durl, format_id, width, height, filesize) in re.findall(r'''(?x)
-                                   <a[^>]+?href="(?P<url>(?:https:)?//[^"]+)".*?
-                                   <strong>(?P<format_id>.+?)</strong>.*?
-                                   <small>(?:(?P<width>\d+)x(?P<height>\d+))?\s+?\((?P<filesize>[\d,]+\s*[GM]iB)\)</small>
-                                ''', webpage):
+        # format (for download) lookup: API
+
+        downloads = self._download_json(
+            'https://massengeschmack.tv/api/v2/downloads/%s' % video_id,
+            video_id,
+            note='Fetching download-optimized formats via API',
+            errnote='Unable to query API',
+            fatal=False,
+            headers={'content-type': 'application/json'})
+
+        files = try_get(downloads, lambda x: x['files'], list) or []
+
+        for file in files:
+            file_url = self._proto_relative_url(file.get('url'))
+            if not file_url:
+                continue
+            file_ext = determine_ext(file_url)
+            file_type = file.get('t')
+            file_size = file.get('size')
+            file_desc = file.get('desc')
+            format_id_list = file_desc.lower().split()
+
+            if file_type == 'film':
+                file_dimensions = file.get('dimensions')
+                resolution = parse_resolution(file_dimensions)
+                width = int_or_none(resolution.get('width'))
+                height =  int_or_none(resolution.get('height')) or int_or_none(file_desc)
+                vcodec = None
+                format_id = ''.join(format_id_list)
+
+            if file_type == 'music':
+                vcodec = 'none'
+                format_id = '-'.join(format_id_list)
+
             formats.append({
-                'url': durl,
-                'format_id': format_id,
-                'width': int_or_none(width),
-                'height': int_or_none(height),
-                'filesize': parse_filesize(filesize),
-                'vcodec': 'none' if format_id.startswith('Audio') else None,
+                'format_id': 'http-' + format_id,
+                'format_note': file_desc,
+                'url': file_url,
+                'ext': file_ext,
+                'filesize': file_size,
+                'width': width,
+                'height': height,
+                'vcodec': vcodec,
             })
 
         self._sort_formats(formats, ('width', 'height', 'filesize', 'tbr'))
 
         return {
-            'id': episode,
+            'id': video_id,
             'title': title,
             'formats': formats,
             'thumbnail': thumbnail,
